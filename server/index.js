c280013c3ae2224a2504176d8ff86f1a231247e2
hesamdarbandi <hesamdarbandi@hotmail.com>
2024-05-27 23:25:57 +0330

    sign transaction

--- server/index.js
+++ server/index.js
@@ -2,35 +2,58 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3042;
+const hashMessage = require('./hashMessage');
+const secp = require("ethereum-cryptography/secp256k1");
+const { toHex } = require("ethereum-cryptography/utils");
 
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "032248680cfccf852fdde25390b21ee6a01f6101e81f630822b367a3dad696a1f8": { balance: 100 , privateKey: "ec0cc29fede8d00dc6de051d5856a8b4e5e8d5b9bdc2ab5416e296ac8cbf6f7e" },
-  "039ea767ad67200bad891557d19c7f573f16a1aea50123081bee9af735275ba9ca": { balance: 60 , privateKey: "9f1c4844c42fc874b6f3eb733af0b8ef7b80365387289658b672fa3d183c1e35" },
-  "0358a050bfe432cac33d3934734e9e024ef6e6075144fb6bbec2289f6bb611a3eb": { balance: 30 , privateKey: "7428d66ff2d6b34b5aee4d326c5cf11df087d48cb8f1828255b495ba38d72f43" },
+  "032248680cfccf852fdde25390b21ee6a01f6101e81f630822b367a3dad696a1f8": { balance: 100, privateKey: "ec0cc29fede8d00dc6de051d5856a8b4e5e8d5b9bdc2ab5416e296ac8cbf6f7e" },
+  "039ea767ad67200bad891557d19c7f573f16a1aea50123081bee9af735275ba9ca": { balance: 60, privateKey: "9f1c4844c42fc874b6f3eb733af0b8ef7b80365387289658b672fa3d183c1e35" },
+  "0358a050bfe432cac33d3934734e9e024ef6e6075144fb6bbec2289f6bb611a3eb": { balance: 30, privateKey: "7428d66ff2d6b34b5aee4d326c5cf11df087d48cb8f1828255b495ba38d72f43" },
 };
 
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
+  if (!balances[address]) {
+    res.send({ balance: 0 });
+  }
   const balance = balances[address].balance || 0;
   res.send({ balance });
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+
+  const { sender, recipient, amount, sign , hash } = req.body;
+
+  const hashMessage = hashMessage(JSON.stringify({ sender, recipient, amount }));
+  if(toHex(hashMessage) != hash){
+      res.status(400).send({ message: "transfer not valid" });
+  }
+
+  var senderPrivateKey = balances[sender].privateKey;
+  var signMessage = secp.secp256k1.sign(messageHash, privateKey);
+
+  if(toHex(signMessage) != sign){
+    res.status(400).send({ message: "transfer not valid" });
+}
+ 
+
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
 
-  if (balances[sender].balance < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
-  } else {
-    balances[sender].balance -= amount;
-    balances[recipient].balance += amount;
-    res.send({ balance: balances[sender].balance });
-  }
+  // if (balances[sender].balance < amount) {
+  //   res.status(400).send({ message: "Not enough funds!" });
+  // } else {
+
+
+  //   balances[sender].balance -= amount;
+  //   balances[recipient].balance += amount;
+  //   res.send({ balance: balances[sender].balance });
+  // }
 });
 
 app.listen(port, () => {
--- server/scripts/generator.js
+++ server/scripts/generator.js
@@ -1,11 +1,23 @@
 const secp = require("ethereum-cryptography/secp256k1");
-const { toHex } = require("ethereum-cryptography/utils");
+const { toHex , utf8ToBytes ,hexToBytes} = require("ethereum-cryptography/utils");
 
-// var privateKey = secp.secp256k1.utils.randomPrivateKey();
-// console.log('private key is :' , toHex(privateKey));
 
-// const publickKey = secp.secp256k1.getPublicKey(privateKey);
-// console.log(toHex(publickKey));
+var privateKey = secp.secp256k1.utils.randomPrivateKey();
+console.log('private key is :', toHex(privateKey));
+
+const publickKey = secp.secp256k1.getPublicKey(privateKey);
+console.log(toHex(publickKey));
+
+const messageHash = "a33321f98e4ff1c283c76998f14f57447545d339b3db534c6d886decb4209f28";
+
+var sign = secp.secp256k1.sign(messageHash, privateKey);
+
+console.log(secp.secp256k1.verify(sign, messageHash, publickKey));
+
+
+
+
+
 
 
 
