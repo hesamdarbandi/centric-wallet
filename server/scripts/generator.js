39b56b18828eef4c1a2b368bf1a09247389dba57
hesamdarbandi <hesamdarbandi@hotmail.com>
2024-05-27 21:00:57 +0330

    sign transaction

---
+++ server/scripts/generator.js
@@ -0,0 +1,12 @@
+const secp = require("ethereum-cryptography/secp256k1");
+const { toHex } = require("ethereum-cryptography/utils");
+
+// var privateKey = secp.secp256k1.utils.randomPrivateKey();
+// console.log('private key is :' , toHex(privateKey));
+
+// const publickKey = secp.secp256k1.getPublicKey(privateKey);
+// console.log(toHex(publickKey));
+
+
+
+
